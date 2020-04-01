dda7e1dc58decb3243e9564f077383912584d1ea
nehadeep <erdeepjaswal@gmail.com>
2020-03-31 22:10:23 -0700

    This version taught us using react class extends 'states' for the version < 16

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> This is my First React app.</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = {
+        persons : [
+            {
+                name: "Hardeep", age: 29
+            },
+            {
+                name: "Sunny" , age: 34
+            }
+        ]
+    };
+
+    switchName = ()=>{
+        console.log("clicked");
+        this.setState({
+            persons : [
+                {
+                    name: "Hardeep1", age: 30
+                },
+                {
+                    name: "Sunny1" , age: 35
+                }
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1> Welcome to my First React app.</h1>
+                </header>
+                <button onClick={this.switchName}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person> My Hobbies</Person>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
