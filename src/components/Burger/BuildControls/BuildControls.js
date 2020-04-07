c1b9721e015db5e4a46028423e011fb4af045a44
nehadeep <erdeepjaswal@gmail.com>
2020-04-07 16:03:04 -0700

    completion of builder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,7 +26,7 @@ const buildControls = (props) =>(
                           disabled={props.disabled[ctrl.type]}/>
         ))}
 
-        <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
+        <button className={classes.OrderButton}  onClick={props.ordered} disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 )
 
